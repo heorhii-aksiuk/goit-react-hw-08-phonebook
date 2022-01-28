330e556907d2b87d8b101878d2031a878be2affd
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-28 19:59:08 +0200

    replace logic from App c-t into ContacntsPage c-t

---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  useFetchContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} from '../store/contacts/contacts-api';
+import { actions } from '../store/contacts/contacts-slice';
+import { selectFilter } from '../store/contacts/contacts-selectors';
+import Section from '../components/Section/Section';
+import Filter from '../components/Filter/Filter';
+import Contacts from '../components/Contacts/Contacts';
+import Form from '../components/Form/Form';
+
+export default function ContactsPage() {
+  const dispatch = useDispatch();
+  const { data: contacts } = useFetchContactsQuery();
+  const [addContact] = useAddContactMutation();
+  const [deleteContact] = useDeleteContactMutation();
+  const filter = useSelector(selectFilter);
+
+  const normalizeFilter = filter.toLowerCase();
+  const filteredContacts = contacts?.filter(contact =>
+    contact.name.toLowerCase().includes(normalizeFilter),
+  );
+
+  const handleFilter = e => dispatch(actions.setFilter(e.target.value));
+
+  return (
+    <>
+      <Section title="Phonebook">
+        <Form onSubmitContact={addContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter value={filter} onChange={handleFilter} />
+        {contacts && (
+          <Contacts contacts={filteredContacts} onDeleteClick={deleteContact} />
+        )}
+      </Section>
+    </>
+  );
+}
