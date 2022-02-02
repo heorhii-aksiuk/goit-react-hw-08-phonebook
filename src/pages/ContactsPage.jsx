7f7ac41601189e8a9d272b11d804ad09058f085c
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-02-02 22:41:54 +0200

    render contancts logic and delete old contacts api

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,44 +1,46 @@
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  useFetchContactsQuery,
-  useAddContactMutation,
-  useDeleteContactMutation,
-} from '../store/contacts/contacts-api';
 import { actions } from '../store/contacts/contacts-slice';
-import { selectFilter } from '../store/contacts/contacts-selectors';
+import {
+  selectContacts,
+  selectFilter,
+} from '../store/contacts/contacts-selectors';
 import Section from '../components/Section/Section';
 import Filter from '../components/Filter/Filter';
 import Contacts from '../components/Contacts/Contacts';
-import AddContactForm from '../components/AddContactForm/AddContactForm';
+// import AddContactForm from '../components/AddContactForm/AddContactForm';
 import contactsOperations from '../store/contacts/contacts-operations';
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
-  const { data: contacts } = useFetchContactsQuery();
-  const [addContact] = useAddContactMutation();
-  const [deleteContact] = useDeleteContactMutation();
+  const contacts = useSelector(selectContacts);
   const filter = useSelector(selectFilter);
 
+  useEffect(() => {
+    dispatch(contactsOperations.fetchContacts());
+  }, [dispatch]);
+
   const normalizeFilter = filter.toLowerCase();
+
+  //надо ли проверка на пустые контакты?
   const filteredContacts = contacts?.filter(contact =>
     contact.name.toLowerCase().includes(normalizeFilter),
   );
 
   const handleFilter = e => dispatch(actions.setFilter(e.target.value));
 
-  dispatch(contactsOperations.fetchContacts());
-
   return (
     <>
       <Section title="Add new contact">
-        <AddContactForm onSubmitContact={addContact} />
+        {/* <AddContactForm onSubmitContact={addContact} /> */}
       </Section>
       <Section title="Contacts">
         <Filter value={filter} onChange={handleFilter} />
         {contacts && (
           <Contacts
+            // contacts={contacts}
             contacts={filteredContacts.reverse()}
-            onDeleteClick={deleteContact}
+            // onDeleteClick={deleteContact}
           />
         )}
       </Section>
--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsApi } from './contacts/contacts-api';
 import { filterReducer, contactsReducer } from './contacts/contacts-slice';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { authReducer } from './auth/auth-slice';
 
 export const store = configureStore({
   reducer: {
-    [contactsApi.reducerPath]: contactsApi.reducer,
     filter: filterReducer,
     auth: authReducer,
     contacts: contactsReducer,
   },
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
-    contactsApi.middleware,
-  ],
+  middleware: getDefaultMiddleware => [...getDefaultMiddleware()],
   devTools: process.env.NODE_ENV === 'development',
 });
 
