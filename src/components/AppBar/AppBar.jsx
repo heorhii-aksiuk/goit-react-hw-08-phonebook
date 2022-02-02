90658441e2d06a636308dccf99ca4810367935d1
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-02-02 20:32:39 +0200

    show or hide AppBar c-t for loggined or not users

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { getIsLoggedIn } from '../../store/auth/auth-selectors';
 import Section from '../Section/Section';
 import UserMenu from '../UserMenu/UserMenu';
 import s from './AppBar.module.css';
+
 export default function AppBar() {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+
   return (
     <Section title="My phonebook">
       <header>
         <nav className={s.nav}>
           <ul>
-            <li className={s.item}>
-              <NavLink to="/contacts" className={s.link}>
-                Contacts
-              </NavLink>
-            </li>
-            <li className={s.item}>
-              <NavLink to="/register" className={s.link}>
-                Register
-              </NavLink>
-            </li>
-            <li className={s.item}>
-              <NavLink to="/login" className={s.link}>
-                Login
-              </NavLink>
-            </li>
+            {isLoggedIn ? (
+              <li className={s.item}>
+                <NavLink to="/contacts" className={s.link}>
+                  Contacts
+                </NavLink>
+              </li>
+            ) : (
+              <>
+                <li className={s.item}>
+                  <NavLink to="/register" className={s.link}>
+                    Register
+                  </NavLink>
+                </li>
+                <li className={s.item}>
+                  <NavLink to="/login" className={s.link}>
+                    Login
+                  </NavLink>
+                </li>
+              </>
+            )}
           </ul>
           <UserMenu></UserMenu>
         </nav>
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getIsLoggedIn, getUserName } from '../../store/auth/auth-selectors';
 import s from './UserMenu.module.css';
 export default function UserMenu() {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  const userName = useSelector(getUserName);
   return (
-    <div>
-      <p className={s.paragraph}>user@mail.com</p>
-      <button className={s.button} type="button">
-        Logout
-      </button>
-    </div>
+    isLoggedIn && (
+      <div>
+        <p className={s.paragraph}>Hello, {userName}</p>
+        <button className={s.button} type="button">
+          Logout
+        </button>
+      </div>
+    )
   );
 }
