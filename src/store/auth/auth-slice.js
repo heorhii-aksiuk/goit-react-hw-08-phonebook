0b2bd73299a90b64418b4f9f24c22e6ad6a88f49
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-02-02 19:22:30 +0200

    add reducer for auth

--- src/store/auth/auth-slice.js
+++ src/store/auth/auth-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import authOperations from './auth-operations';
 const initialState = {
   user: { name: null, email: null, password: null },
   token: null,
@@ -9,7 +9,13 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {},
+  extraReducers: {
+    [authOperations.register.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+  },
 });
 
 export const authReducer = authSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from './contacts/contacts-api';
 import { filterReducer } from './contacts/contacts-slice';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { authReducer } from './auth/auth-slice';
 
 export const store = configureStore({
   reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
     filter: filterReducer,
+    auth: authReducer,
   },
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware(),
